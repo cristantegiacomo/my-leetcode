002d4493a4a95ef6694a20b7cc2c191d836b5178
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-07-28 13:33:39 +0200

    Time: 0 ms (100%), Space: 20.5 MB (17.34%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp={}
+        res=[]
+
+        for i, n in enumerate(nums):
+            mp[n]=i
+
+        for i, n in enumerate(nums):
+            diff=target-n
+            if diff in mp and mp[diff]!=i:
+                return [i, mp[diff]]
+        return []
+
+# soluzione con hashmap
