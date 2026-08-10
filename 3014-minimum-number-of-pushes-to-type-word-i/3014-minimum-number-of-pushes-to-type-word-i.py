cfa9b9e531137381e8fb72b2b2e9497189a80204
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-10 12:00:23 +0200

    Time: 0 ms (100%), Space: 19.3 MB (53.76%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -1,11 +1,8 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        lung = len(word)
-        giri = lung // 8
-        resto = lung % 8
-        somma = i = 0
-
-        for i in range(giri):
-            somma = somma + i + 1
-
-        return 8*somma + (giri+1)*resto
+        res = 0
+        k = 1
+        for i in range(len(word)):
+            if i != 0 and i % 8 == 0: k+= 1
+            res += k
+        return res
