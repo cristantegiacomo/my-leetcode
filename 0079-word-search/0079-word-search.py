296c413a70ce60b81d5378da4e92ffe4ebb71ab8
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-11 12:14:41 +0200

    Time: 4377 ms (28.04%), Space: 19.5 MB (26.82%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,8 +2,9 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
         n = len(board[0])
+        sett = set()
 
-        def dfs(i, j, sett, idx):
+        def dfs(i, j, idx):
             if idx == len(word):
                 return True
 
@@ -17,15 +18,15 @@ class Solution:
                 return False
 
             sett.add((i,j))
-            dx = dfs(i+1, j, sett, idx+1) # destra
-            sx = dfs(i-1, j, sett, idx+1) # sinistra
-            up = dfs(i, j+1, sett, idx+1) # up
-            down = dfs(i, j-1, sett, idx+1) # down
+            res = (dfs(i+1, j, idx+1) or 
+                   dfs(i-1, j, idx+1) or 
+                   dfs(i, j+1, idx+1) or 
+                   dfs(i, j-1, idx+1))
             sett.remove((i,j))
-            return dx or sx or up or down
+            return res
         
-        var = False
         for j in range(m):
             for i in range(n):
-                var = var or dfs(i, j, set(), 0)
-        return var
+                if dfs(i, j, 0):
+                    return True
+        return False
