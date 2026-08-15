e2f6436be9a3a7098f049160cb0fbb3ebfb561ed
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-15 23:52:39 +0200

    Time: 4 ms (15.57%), Space: 19.5 MB (6.65%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counts = Counter(s)
+        tot = foundOdd = 0
+
+        for freq in counts.values():
+            val = freq - freq % 2
+            if freq%2 != 0:
+                foundOdd = 1
+            tot += val
+
+        return tot + foundOdd
