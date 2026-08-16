120e9584afdc024cb27dfe9f1d8858d5a2b5ce95
Giacomo Cristante <cristante.giacomo@gmail.com>
2026-08-16 15:41:51 +0200

    Time: 3 ms (61.59%), Space: 19.4 MB (11.21%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,16 +1,14 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         counts = Counter(nums)
-        maxf = max(counts.values())
-        bucket = [ [] for _ in range(maxf+1) ]
         res = []
 
-        for n, freq in counts.items():
-            bucket[freq].append(n)
+        minHeap = [ (freq, -n) for n, freq in counts.items() ]
+        heapq.heapify(minHeap)
 
-        for i in range(1, len(bucket)):
-            bucket[i].sort(reverse=True)
-            for j in range(len(bucket[i])):
-                for _ in range(i):
-                    res.append(bucket[i][j])
+        while minHeap:
+            freq, num = heapq.heappop(minHeap)
+            for _ in range(freq):
+                res.append(-num)
+
         return res
